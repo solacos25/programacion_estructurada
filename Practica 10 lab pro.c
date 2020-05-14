2bfdc3303136cb1fbef83ca035e67c30ba540980
solacos25 <60495701+solacos25@users.noreply.github.com>
2020-05-14 17:29:10 -0500

    Practica 10 jueves 30 de abril

---
+++ Practica 10 lab pro.c
@@ -0,0 +1,243 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Darle valor a un vector de 10 estudiantes y leerlo despues 
+Pedir datos de 10 estudiantes y almacenarlos en matrices, cada estudiante tiene 3 matrices de 2*2*/
+
+int main() {
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	return 0;	
+	}
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	/*Sorry prof, no supe y no entendi como hacerlo, lo intente mucho y de muchas formas, lo seguire intentando hacer*/
