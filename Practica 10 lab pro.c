92549533da6fc871e417e246d62092c437b3ba67
solacos25 <60495701+solacos25@users.noreply.github.com>
2020-05-14 17:40:39 -0500

    Practica 10 jueves 30 de abril

---
+++ Practica 10 lab pro.c
@@ -0,0 +1,223 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Darle valor a un vector de 10 estudiantes y leerlo despues 
+Pedir datos de 10 estudiantes y almacenarlos en matrices, cada estudiante tiene 3 matrices de 2*2*/
+
+int main() {
+	
+	char nombre[2][2], datos[2][2], informacion[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion[k][2]);
+		
+		printf("\n%c",nombre[i][2], datos[j][2], informacion[k][2]);
+		}	
+	}
+	
+	return 0;	
+	}
+	}
+	
+	char nombre2[2][2], datos2[2][2], informacion2[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre2[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos2[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion2[k][2]);
+		
+		printf("\n%c",nombre2[i][2], datos2[j][2], informacion2[k][2]);
+		}	
+	}
+	
+	char nombre3[2][2], datos3[2][2], informacion3[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre3[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos3[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion3[k][2]);
+		
+		printf("\n%c",nombre3[i][2], datos3[j][2], informacion3[k][2]);
+		}	
+	}
+	
+	char nombre4[2][2], datos4[2][2], informacion4[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre4[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos4[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion4[k][2]);
+		
+		printf("\n%c",nombre4[i][2], datos4[j][2], informacion4[k][2]);
+		}	
+	}
+	
+	char nombre5[2][2], datos5[2][2], informacion5[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre5[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos5[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion5[k][2]);
+		
+		printf("\n%c",nombre5[i][2], datos5[j][2], informacion5[k][2]);
+		}	
+	}
+	
+	char nombre6[2][2], datos6[2][2], informacion6[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre6[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos6[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion6[k][2]);
+		
+		printf("\n%c",nombre6[i][2], datos6[j][2], informacion6[k][2]);
+		}	
+	}
+	
+	char nombre7[2][2], datos7[2][2], informacion7[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre7[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos7[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion7[k][2]);
+		
+		printf("\n%c",nombre7[i][2], datos7[j][2], informacion7[k][2]);
+		}	
+	}
+	
+	char nombre8[2][2], datos8[2][2], informacion8[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre8[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos8[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion8[k][2]);
+		
+		printf("\n%c",nombre8[i][2], datos8[j][2], informacion8[k][2]);
+		}	
+	}
+	
+	char nombre9[2][2], datos9[2][2], informacion9[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre9[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos9[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion9[k][2]);
+		
+		printf("\n%c",nombre9[i][2], datos9[j][2], informacion9[k][2]);
+		}	
+	}
+	
+	char nombre10[2][2], datos10[2][2], informacion10[2][2];
+	int i;
+	for (i=0;i<2;i++) 
+	{
+		printf("\n Ingrese nombre, segundo nombre, primer apellido y segundo apellido:");
+		scanf("%c", &nombre10[i][2]);
+		
+		int j;
+		for (j=0;j<2;j++){
+		printf("\n Ingrese genero, carrera, facultad, lugar:");
+		scanf("%c", &datos10[j][2]);
+	
+		int k;
+		for (k=0;k<2;k++){
+		printf("\n Ingrese fecha de nacimiento, semestre, turno, matricula:");
+		scanf("%c", &informacion10[k][2]);
+		
+		printf("\n%c",nombre10[i][2], datos10[j][2], informacion10[k][2]);
+		}	
+	}
+	
+	/*Sorry prof, no supe y no entendi como hacerlo, lo intente mucho y de muchas formas, lo seguire intentando hacer*/
